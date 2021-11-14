31a8e4d9a8a2300e6209c952a5874129e7cebebd
Preston Gardner <ptg5081@psu.edu>
2021-11-14 13:04:40 -0500

    main loop motor speed polling

--- capstonecodes-version3.0/capstonecodes-version3.0.ino
+++ capstonecodes-version3.0/capstonecodes-version3.0.ino
@@ -1,40 +1,48 @@
-/*
-SparkFun Inventor's Kit for Arduino
-Example sketch 12
+#define USING_16MHZ     true
+#define USING_8MHZ      false
+#define USING_250KHZ    false
 
-SparkFun Motor Driver
 
-Use the SparkFun Motor Driver to control the speed and direction of a motor
+#define USE_TIMER_0     false
+#define USE_TIMER_1     true
+#define USE_TIMER_2     false
+#define USE_TIMER_3     false
 
-This sketch was written by SparkFun Electronics,
-with lots of help from the Arduino community.
-This code is completely free for any use.
-Visit http://learn.sparkfun.com/products/2 for SIK information.
-Visit http://www.arduino.cc to learn more about Arduino.
-*/
 #include <LiquidCrystal.h>
+#include "megaAVR_TimerInterrupt.h"
+
+#define TIMER1_INTERVAL_MS        50L
 
 //define the two direction logic pins and the speed / PWM pin
 const int DIR_A = 5;
 const int DIR_B = 4;
 const int PWM = 6;
+const int BUTTON_PIN = 13;
+const int ENC_PIN = 2;
+
+double motor_speed = 0; // measured motor speed steps/millisec (12 steps / input shaft rotation, 986.41 input shaft rotations / output shaft rotations)
+
+long enc_count = 0; //keeps track of the rotations of the motor
+
+long last_count = 0; //last checked motor position
+long last_check = 0;//last time motor pos was checked
+
 LiquidCrystal lcd (12, 11, 10, 9, 8, 7);
 
 bool running = false;
 bool wasRunning = false;
 bool reset = false;
-int count = 0;
-int save = 0;
-double motorSpeed = 50;
-//double motorSpeed = 2.78;
-//double motorSpeed = 1.1; 
+
+double pwm_lvl = 30; //pwm level fed to the motor
+//double pwm_lvl = 2.78;
+//double pwm_lvl = 1.1; 
 unsigned long pausedTime = 0;
 unsigned long paused = 0;
 unsigned long elapsedTime = 0;
 String percentStr = String("0%");
 
 long button_press = -1;
-long debounce_time = 500;
+const long debounce_time = 250;
 
 long hour = 0, minute = 59, second = 59;//second 
 long totaltime = 3600;
@@ -42,40 +50,38 @@ long countdown_time = (hour*3600) + (minute * 60) + second;
 
 void setup()
 {
+  lcd.begin (16, 2);
+  lcd.print ("Initializing");
   //set all pins as output
   pinMode(DIR_A, OUTPUT);
   pinMode(DIR_B, OUTPUT);
   pinMode(PWM, OUTPUT);
 
    // set the number of columns and the number of lines of lcd
-  lcd.begin (16, 2);
   running = false;
   wasRunning = false;
   reset = false;
-  count = 0;
-  percentStr = String("0%");
-  // I see the message on the display
-  lcd.print ("Initializing");
   reset = false;
-  delay(500);
   lcd.clear();
   lcd.print("Press button to");
   lcd.setCursor(0,1);//write the rest on new line
   lcd.print("begin.");
   Serial.begin(9600);//initialize serial output for debugging FIXME
-  attachInterrupt(digitalPinToInterrupt(13), toggleDelivery, FALLING); //attach button interupt to pin D2
+  attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), toggleDelivery, FALLING); //attach button interupt 
+  attachInterrupt(digitalPinToInterrupt(ENC_PIN), updateEncoder, FALLING); //attach button interupt 
 }
 
 void loop()
 {
  if(running){
     if(!wasRunning){
-      //drive forward at full speed by pulling DIR_A High
-      //and DIR_B low, while writing a full 255 to PWM to 
-      //control speed
+      last_check = millis();
+      last_count = enc_count;
+      motor_speed = 0;
+
       digitalWrite(DIR_A, HIGH);
       digitalWrite(DIR_B, LOW);
-      analogWrite(PWM, motorSpeed);
+      analogWrite(PWM, pwm_lvl);
       lcd.clear();
       lcd.print("|>");
       lcd.setCursor(0,1);
@@ -84,18 +90,27 @@ void loop()
       running = true;
       wasRunning = true; 
     }
+    else{
+      long time = millis() - last_check;
+      long steps = enc_count - last_count;
+      if(steps > 0){ //only update motor speed if a rotation has happened
+        motor_speed = (double)steps/time;
+        last_count = enc_count;
+        last_check = millis();
+      }
+
+      // Serial.println(motor_speed, 6); //FIXME      
+      Serial.println(time);
+    }
 
     elapsedTime = millis() - pausedTime;
     long countdowntime_seconds = countdown_time - (elapsedTime / 1000);
     
     if(countdowntime_seconds == 0){
-      //drive forward at full speed by pulling DIR_A High
-      //and DIR_B low, while writing a full 255 to PWM to 
-      //control speed
       digitalWrite(DIR_A, LOW);
       digitalWrite(DIR_B, LOW);
-      analogWrite(PWM, motorSpeed);
-      attachInterrupt(digitalPinToInterrupt(13), toggleReset, FALLING); //attach button interupt to pin D2
+      analogWrite(PWM, pwm_lvl);
+      attachInterrupt(digitalPinToInterrupt(BUTTON_PIN), toggleReset, FALLING); //attach button interupt to pin D2
       lcd.clear();
       lcd.print("Complete, press");
       lcd.setCursor(0,1);
@@ -133,7 +148,7 @@ void loop()
     //control speed
     digitalWrite(DIR_A, LOW);
     digitalWrite(DIR_B, LOW);
-    analogWrite(PWM, motorSpeed);
+    analogWrite(PWM, pwm_lvl);
     paused = millis();
     lcd.clear();
     lcd.print("|| Press button");
@@ -156,5 +171,13 @@ void toggleReset(){
     button_press = millis();
   }
 }
+
+void updateEncoder(){
+  enc_count++;
+  //Serial.println(enc_count);//FIXME
+}  
   
-  
+void TimerHandler1()
+{
+  // Doing something here inside ISR
+}
